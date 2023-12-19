84f139f345904fe8db379711c69efdba3c731a4b
Eunice-Serani <eaamable91@gmail.com>
2023-12-19 06:41:16 -0800

    Bubble sorting

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * swap_node - Swaps a node for his previous one
+ * swap_node - Swaps a node for its previous one
  * @node: node
  * @list: node list
  * Return: return a pointer to a node which was to enter it
@@ -11,19 +11,19 @@ listint_t *swap_node(listint_t *node, listint_t **list)
 {
 	listint_t *back = node->prev, *current = node;
 
-
 	back->next = current->next;
 	if (current->next)
 		current->next->prev = back;
-	current->next = back;
-	current->prev = back->prev;
-	back->prev = current;
+		current->next = back;
+		current->prev = back->prev;
+		back->prev = current;
 	if (current->prev)
 		current->prev->next = current;
 	else
-		*list = current;
+	*list = current;
 	return (current);
 }
+
 /**
  * cocktail_sort_list - This is a cocktail sort implementation
  * working on a double linked lists
@@ -34,12 +34,14 @@ void cocktail_sort_list(listint_t **list)
 	listint_t *node;
 	int swap_done = 1;
 
-	if (*list == '\0' || *list == '\0' || *list->next == '\0')
+	if (*list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
+
 	node = *list;
 	while (swap_done == 1)
 	{
 		swap_done = 0;
+
 		while (node->next)
 		{
 			if (node->n > node->next->n)
@@ -48,7 +50,8 @@ void cocktail_sort_list(listint_t **list)
 				swap_done = 1;
 				print_list(*list);
 			}
-			node = node->next;
+			else
+				node = node->next;
 		}
 		if (swap_done == 0)
 			break;
