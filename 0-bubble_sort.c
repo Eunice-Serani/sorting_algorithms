220256b9a32c5910d79090ab6dae025b6e3e0253
Eunice-Serani <eaamable91@gmail.com>
2023-12-19 03:46:05 -0800

    Bubble sorting

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,11 +9,11 @@
 void bubble_sort(int *array, size_t size)
 {
 
-	size_t i, index, tmp = 0;
+	size_t j, index, tmp = 0;
 
 	if (size < 2)
 		return;
-	for (i = 0; i < size; i++)
+	for (j = 0; j < size; j++)
 		for (index = 0; index < size; index++)
 		{
 			if (array[index] > array[index + 1] && array[index + 1])
