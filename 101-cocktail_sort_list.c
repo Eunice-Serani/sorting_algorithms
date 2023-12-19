b553fd5929e009e60e601df9e0d5b6d89d06bec3
Eunice-Serani <eaamable91@gmail.com>
2023-12-19 06:48:22 -0800

    Cocktail sorting

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -14,9 +14,9 @@ listint_t *swap_node(listint_t *node, listint_t **list)
 	back->next = current->next;
 	if (current->next)
 		current->next->prev = back;
-		current->next = back;
-		current->prev = back->prev;
-		back->prev = current;
+	current->next = back;
+	current->prev = back->prev;
+	back->prev = current;
 	if (current->prev)
 		current->prev->next = current;
 	else
